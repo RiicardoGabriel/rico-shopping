830c908e10f49b18484734c48e7e67bd64aef02f
Ricardo Gabriel <riicardogabriel12@gmail.com>
2022-06-04 17:59:01 -0300

    Feito 2ª requisito

--- script.js
+++ script.js
@@ -12,10 +12,10 @@ const createCustomElement = (element, className, innerText) => {
   return e;
 };
 
-const createProductItemElement = ({ sku, name, image }) => {
+const createProductItemElement = ({ id: sku, title: name, thumnail: image }) => {
   const section = document.createElement('section');
   section.className = 'item';
-
+  document.querySelector('.items').appendChild(section);
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
@@ -23,6 +23,11 @@ const createProductItemElement = ({ sku, name, image }) => {
 
   return section;
 };
+
+function exibeDados(item) {
+  fetchProducts(item).then((itens) =>
+  itens.results.forEach((items) => createProductItemElement(items)));
+}
 
 const getSkuFromProductItem = (item) => item.querySelector('span.item__sku').innerText;
 
@@ -38,4 +43,6 @@ const createCartItemElement = ({ sku, name, salePrice }) => {
   return li;
 };
 
-window.onload = () => { };
+window.onload = () => { 
+  exibeDados('computador');
+ };
